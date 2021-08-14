1bacd1b9be55c02eaada12249c76777b2a440813
therossington <camsymonds@outlook.com>
2021-08-14 18:28:16 +1000

    Finish game

--- game_script.js
+++ game_script.js
@@ -23,57 +23,71 @@ let roundsPlayed = 0;
 //Plays a round.
 
 const container = document.querySelector('.score-container');
-const selections = document.createElement('h2');
+const selections = document.createElement('h3');
+const result = document.createElement('h3');
+const score = document.createElement('h3');
+const playerScore = document.createElement('h3');
+const winAnnounce = document.createElement('h1');
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
+        selections.textContent = `Computer selects ${computerSelection}`;
+        container.appendChild(selections);
+        result.textContent = "It was a tie.";
+        container.appendChild(result);
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        selections.textContent = `Computer selects ${computerSelection}`;
+        container.appendChild(selections);   
+        playerWins++;
+        result.textContent = "You win! Rock beats Scissors.";
+        container.appendChild(result);
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        selections.textContent = `Computer selects ${computerSelection}`;
+        container.appendChild(selections);
+        playerWins++;
+        result.textContent = "You win! Paper beats Rock.";
+        container.appendChild(result);
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         selections.textContent = `Computer selects ${computerSelection}`
         container.appendChild(selections);
-        return "It was a tie.";
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        console.log("Player selects "+playerSelection)
-        console.log("Computer selects "+computerSelection)
-        return "You win! Rock beats Scissors.";
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        console.log("Player selects "+playerSelection)
-        console.log("Computer selects "+computerSelection)
-        return "You win! Paper beats Rock.";
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        console.log("Player selects "+playerSelection)
-        console.log("Computer selects "+computerSelection)
-        return "You win! Scissors beats Paper.";
+        playerWins++
+        result.textContent = "You win! Scissors beats Paper.";
+        container.appendChild(result);
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        console.log("Player selects "+playerSelection)
-        console.log("Computer selects "+computerSelection)
-        return "You lose! Rock beats Scissors.";
+        selections.textContent = `Computer selects ${computerSelection}`
+        container.appendChild(selections);
+        computerWins++
+        result.textContent = "You lose! Rock beats Scissors.";
+        container.appendChild(result);
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        console.log("Player selects "+playerSelection)
-        console.log("Computer selects "+computerSelection)
-        return "You lose! Paper beats Rock.";
+        selections.textContent = `Computer selects ${computerSelection}`
+        container.appendChild(selections);
+        computerWins++
+        result.textContent = "You lose! Paper beats Rock.";
+        container.appendChild(result);
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        console.log("Player selects "+playerSelection)
-        console.log("Computer selects "+computerSelection)
-        return "You lose! Scissors beats Papers.";
+        selections.textContent = `Computer selects ${computerSelection}`
+        container.appendChild(selections);
+        computerWins++
+        result.textContent = "You lose! Scissors beats Papers.";
+        container.appendChild(result);
     }
+    playerScore.textContent = `Player score = ${playerWins}`
+    score.textContent = `Computer score = ${computerWins}`
+    container.appendChild(score);
+    container.appendChild(playerScore);
+    if (playerWins === 5) {
+        winAnnounce.textContent = "Player wins!";
+        container.appendChild(winAnnounce)
+        } else if (computerWins === 5) {
+            winAnnounce.textContent = "Computer wins!";
+        } else if (playerWins === 5 && computerWins === 5) {
+            winAnnounce.textContent = "It's a tie!";
+            container.appendChild(winAnnounce)
+        };
 }
 
 const btn = document.querySelectorAll('button');
 btn.forEach(btn => btn.addEventListener('click', () => {
     console.log(playRound(btn.value, getComputerSelection()))
 }));
-
-// Five round game (not looped).
-
-for (let i = 0; i <= 5; i++) {
-    console.log("Player score = "+playerWins)
-    console.log("Computer score = "+computerWins)
-    if (roundsPlayed == 5 || playerWins + computerWins == 5) {
-        if (playerWins > computerWins) {
-            return "Player wins!"
-        } else if (playerWins < computerWins) {
-            return "Computer wins!"
-        } else if (playerWins == computerWins) {
-            return "It's a tie!"
-        }
-    }
-}  
